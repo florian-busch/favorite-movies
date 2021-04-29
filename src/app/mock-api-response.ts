e89f4f6bca3136ee46006829fed350a03ce9fc38
florian-busch <72664648+florian-busch@users.noreply.github.com>
2021-04-29 22:42:33 +0200

    Commit

---
+++ src/app/mock-api-response.ts
@@ -0,0 +1,355 @@
+import { Movie } from './movie';
+
+
+export const MOVIES: Movie = {
+  "page": 1,
+  "results": [
+      {
+          "adult": false,
+          "backdrop_path": "/bxY7ve1LP8atCIuvr4jeeJMmU4w.jpg",
+          "genre_ids": [
+              35,
+              18
+          ],
+          "id": 37165,
+          "original_language": "en",
+          "original_title": "The Truman Show",
+          "overview": "Truman Burbank is the star of The Truman Show, a 24-hour-a-day reality TV show that broadcasts every aspect of his life without his knowledge. His entire life has been an unending soap opera for consumption by the rest of the world. And everyone he knows, including his wife and his best friend is really an actor, paid to be part of his life.",
+          "popularity": 22.497,
+          "poster_path": "/dOmxN9xhLITi43j2xGFEcsgv7QM.jpg",
+          "release_date": "1998-06-04",
+          "title": "The Truman Show",
+          "video": false,
+          "vote_average": 8.1,
+          "vote_count": 13063
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/iUAFLqAj7p1v8zkOReyd7tjSLDg.jpg",
+          "genre_ids": [
+              18
+          ],
+          "id": 341744,
+          "original_language": "es",
+          "original_title": "Truman",
+          "overview": "Tomás, who lives in Canada, travels to Madrid, Spain, to visit his old friend Julián. Both of them, accompanied by Truman, Julián's faithful dog, will share many surprising and emotional little moments, triggered by the hard situation Julián is going through, for just a few days.",
+          "popularity": 7.349,
+          "poster_path": "/nH7L2zNPGUGlrSUl7KHI9Xxup4s.jpg",
+          "release_date": "2015-10-30",
+          "title": "Truman",
+          "video": false,
+          "vote_average": 7,
+          "vote_count": 268
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/pmsfi7seRvhj5FpR1T1USD08txG.jpg",
+          "genre_ids": [
+              99
+          ],
+          "id": 730081,
+          "original_language": "en",
+          "original_title": "Truman & Tennessee: An Intimate Conversation",
+          "overview": "Two of the greatest writers of the past century examined in a dialogue that stretches from their early days of friendship to their final, unsparing critiques of each other. They lived parallel lives and struggled with a lifelong pursuit of creativity, self-doubt, addiction, and success.",
+          "popularity": 28.216,
+          "poster_path": "/3h12qdFFPARmIglVmLuOCkMvQFA.jpg",
+          "release_date": "2021-04-30",
+          "title": "Truman & Tennessee: An Intimate Conversation",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/xZRuns3IlPxnzpHhbscAOLyiwNr.jpg",
+          "genre_ids": [
+              18,
+              10770
+          ],
+          "id": 16901,
+          "original_language": "en",
+          "original_title": "Truman",
+          "overview": "Biographical account of America's President for the latter part of WWII. Shows Truman's rise from small-town nobody to leader of the USA, his decision to use the Atomic Bomb against Japan, and subsequent election as the US' post-war President.",
+          "popularity": 3.796,
+          "poster_path": "/4SW6da4XjxLGlRVS079O1ORpjqk.jpg",
+          "release_date": "1995-09-09",
+          "title": "Truman",
+          "video": false,
+          "vote_average": 7.1,
+          "vote_count": 13
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              99
+          ],
+          "id": 298662,
+          "original_language": "en",
+          "original_title": "A Visit with Truman Capote",
+          "overview": "'With Love from Truman' portrays an intimate meeting with renowned author Truman Capote. As a reporter interviews him in his beachfront home, Capote shares his \"self-regarding\" personality through hip philosophy and calculated jokes. He offers insights in an endearingly raspy voice about his latest book, 'In Cold Blood', which Capote declares to be part of a new genre, the \"non-fiction novel.\" Just as the Maysles brothers' direct cinema classics turn real stories into narratives, Capote's non-fiction novel makes an effort to turn reality into art. In Cold Blood is based on first-hand accounts of an actual murder. The author affectionately discusses his coverage of the subsequent trial and his intriguing relationship with the two young killers. Capote claims it is the spontaneity of life that compels him to portray reality, but it is his own fresh energy and startling sense of humor that keep us intrigued.",
+          "popularity": 1.938,
+          "poster_path": null,
+          "release_date": "1966-01-01",
+          "title": "A Visit with Truman Capote",
+          "video": false,
+          "vote_average": 6.1,
+          "vote_count": 5
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              18,
+              10770
+          ],
+          "id": 263221,
+          "original_language": "en",
+          "original_title": "Harry S. Truman: Plain Speaking",
+          "overview": "A biography of President Harry S. Truman.",
+          "popularity": 1.514,
+          "poster_path": "/7EzzuZQEHqjOwMoV1T4J37VqPkg.jpg",
+          "release_date": "1976-10-06",
+          "title": "Harry S. Truman: Plain Speaking",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              18
+          ],
+          "id": 786749,
+          "original_language": "en",
+          "original_title": "Truman Syndrome",
+          "overview": "A person who believes they're being watched all day, every day, struggles with their mental health and their grip on reality.",
+          "popularity": 0.6,
+          "poster_path": "/f3lrhJr7M8pk1A0ljUzrnUgsjCP.jpg",
+          "release_date": "2021-01-21",
+          "title": "Truman Syndrome",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              99
+          ],
+          "id": 365256,
+          "original_language": "en",
+          "original_title": "American Experience: Truman",
+          "overview": "He was a farmer, a businessman, an unknown politician who suddenly found himself president. Of all the men who had held the highest office, Harry Truman was the least prepared, but would prove to be a surprise.",
+          "popularity": 0.6,
+          "poster_path": null,
+          "release_date": "1997-01-01",
+          "title": "American Experience: Truman",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              99
+          ],
+          "id": 670673,
+          "original_language": "de",
+          "original_title": "Truman Capote - Enfant terrible der amerikanischen Literatur",
+          "overview": "",
+          "popularity": 1.138,
+          "poster_path": "/1zS7gZUetZdojLEA3kwJOZThbQr.jpg",
+          "release_date": "2016-11-16",
+          "title": "Truman Capote - Enfant terrible der amerikanischen Literatur",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [],
+          "id": 810559,
+          "original_language": "en",
+          "original_title": "David Susskind Archive: Truman Capote Tells All",
+          "overview": "An extensive interview originally broadcast in Feb. 1979. Host David Susskind and Truman Capote discuss the icon's history, his writing, his social persona and impact. More than an interview, the wide-ranging conversation between longtime friends delves into topics you are unlikely to see elsewhere.",
+          "popularity": 0.6,
+          "poster_path": null,
+          "release_date": "",
+          "title": "David Susskind Archive: Truman Capote Tells All",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/xLANFjyrdj0qUeQUkaFUDkXgrJ.jpg",
+          "genre_ids": [
+              99
+          ],
+          "id": 594957,
+          "original_language": "fr",
+          "original_title": "Staline-Truman, l'aube de la guerre froide",
+          "overview": "",
+          "popularity": 0.6,
+          "poster_path": "/nSgkcYxPTVucOXdsjfCjxTWytKn.jpg",
+          "release_date": "2019-04-04",
+          "title": "Staline-Truman, l'aube de la guerre froide",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [],
+          "id": 742930,
+          "original_language": "en",
+          "original_title": "How's It Going to End - The Making of 'The Truman Show'",
+          "overview": "The making of 'The Truman Show'.",
+          "popularity": 0.6,
+          "poster_path": null,
+          "release_date": "",
+          "title": "How's It Going to End - The Making of 'The Truman Show'",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/kJ765BgJ3jVdkav5qylNV1NyGWW.jpg",
+          "genre_ids": [
+              80,
+              18
+          ],
+          "id": 398,
+          "original_language": "en",
+          "original_title": "Capote",
+          "overview": "A biopic of the writer, Truman Capote and his assignment for The New Yorker to write the non-fiction book, 'In Cold Blood'.",
+          "popularity": 10.23,
+          "poster_path": "/k7z0tDxzbDNev7F8l3Y56Pk5seR.jpg",
+          "release_date": "2005-09-30",
+          "title": "Capote",
+          "video": false,
+          "vote_average": 7,
+          "vote_count": 1010
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/sX5mm8Lw1jEEq1ZSgJOzbutQnLE.jpg",
+          "genre_ids": [
+              99
+          ],
+          "id": 621769,
+          "original_language": "en",
+          "original_title": "The Capote Tapes",
+          "overview": "Newly discovered interviews with friends of Truman Capote made by Paris Review co-founder George Plimpton invigorate this fascinating documentary on the author (and socialite) behind Breakfast at Tiffany’s and In Cold Blood, while situating Capote in the 20th-century American literary canon.",
+          "popularity": 7.002,
+          "poster_path": "/1nYpin70AExhmOjmULhBlHxDL2u.jpg",
+          "release_date": "2019-09-07",
+          "title": "The Capote Tapes",
+          "video": false,
+          "vote_average": 7.5,
+          "vote_count": 2
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/2Fa1IO7qcXX9KlnocFyhPNUxRwb.jpg",
+          "genre_ids": [
+              18,
+              10751
+          ],
+          "id": 147135,
+          "original_language": "en",
+          "original_title": "One Christmas",
+          "overview": "Based on Truman Capote's bittersweet tale of a young boy's adventures with the father he's never known in New Orleans in the 1930s..",
+          "popularity": 4.471,
+          "poster_path": "/uTVZyFVVJ5WVjH1G08UxeKSv9tw.jpg",
+          "release_date": "1994-12-19",
+          "title": "One Christmas",
+          "video": false,
+          "vote_average": 5.2,
+          "vote_count": 6
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              18
+          ],
+          "id": 198781,
+          "original_language": "en",
+          "original_title": "Trilogy",
+          "overview": "Trilogy is an anthology film of three adaptations of Truman Capote short stories: Miriam, Among the Paths to Eden and A Christmas Memory. It was listed to compete at the 1968 Cannes Film Festival, but the festival was cancelled due to the events of May 1968 in France.",
+          "popularity": 1.653,
+          "poster_path": "/wG10FcHkLTAD7jmcCZ4RgirosWO.jpg",
+          "release_date": "1969-06-08",
+          "title": "Trilogy",
+          "video": false,
+          "vote_average": 5.7,
+          "vote_count": 3
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/gQYsE1Zu89fbi3yUL4bubxoRjbh.jpg",
+          "genre_ids": [
+              80,
+              18,
+              36
+          ],
+          "id": 18900,
+          "original_language": "en",
+          "original_title": "In Cold Blood",
+          "overview": "After a botched robbery results in the brutal murder of a rural family, two drifters elude police, in the end coming to terms with their own mortality and the repercussions of their vile atrocity.",
+          "popularity": 6.949,
+          "poster_path": "/hXRyR0kfB64aj2y76JBjRSRQYOw.jpg",
+          "release_date": "1967-12-15",
+          "title": "In Cold Blood",
+          "video": false,
+          "vote_average": 7.6,
+          "vote_count": 222
+      },
+      {
+          "adult": false,
+          "backdrop_path": null,
+          "genre_ids": [
+              18
+          ],
+          "id": 62952,
+          "original_language": "en",
+          "original_title": "A Christmas Memory",
+          "overview": "Narrated by Truman Capote. Emmy Award winning adaptation of Capote's recollection of his youth in the rural South during the Depression. Later released in the anthology film Trilogy (1969) along with two other Capote adaptations by Frank and Eleanor Perry.",
+          "popularity": 0.927,
+          "poster_path": "/2UWsJ7CNzSUEiKtHu878HkauXMY.jpg",
+          "release_date": "1966-12-21",
+          "title": "A Christmas Memory",
+          "video": false,
+          "vote_average": 0,
+          "vote_count": 0
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/kA28hzGAais4Rlk0RG6PsQzTqU4.jpg",
+          "genre_ids": [
+              18
+          ],
+          "id": 9672,
+          "original_language": "en",
+          "original_title": "Infamous",
+          "overview": "While researching his book In Cold Blood, writer Truman Capote develops a close relationship with convicted murderers Dick Hickock and Perry Smith.",
+          "popularity": 9.752,
+          "poster_path": "/cIQUn20lUQpi70W0Z7ixezQJ7gj.jpg",
+          "release_date": "2006-09-11",
+          "title": "Infamous",
+          "video": false,
+          "vote_average": 6.6,
+          "vote_count": 133
+      }
+  ],
+  "total_pages": 1,
+  "total_results": 19
+}
